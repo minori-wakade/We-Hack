2e9372c0c3fe46ed7a8c4a3ce0d24e0ec0b94927
Minori Wakade <minoriwakade@gmail.com>
2025-03-23 07:10:28 +0530

    Final Commit

--- ContactUs/contact.js
+++ ContactUs/contact.js
@@ -24,3 +24,29 @@ const navLinks = document.getElementById("navLinks");
 navToggle.addEventListener("click", () => {
     navLinks.classList.toggle("show");
 });
+//Email
+(function(){
+    emailjs.init("qeE9QIMiu9sPb7Rsw"); // Replace with your actual Public Key
+ })();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Get user input values
+    let name = document.getElementById("first-name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    // Sending email via EmailJS
+    emailjs.send("service_am66nnr", "template_plfs07q", {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    }).then(function(response) {
+        document.getElementById("response-message").innerText = "Email sent successfully!";
+    }, function(error) {
+        document.getElementById("response-message").innerText = "Failed to send email!";
+    });
+});
